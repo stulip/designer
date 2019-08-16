3d36d75850366a2e095c050c77692c5a2683bc62
tangzehua <stulip@126.com>
2019-08-16 15:57:41 +0800

    添加标尺测试

--- modules/fr-web/src/pages/home/Home.js
+++ modules/fr-web/src/pages/home/Home.js
@@ -3,17 +3,102 @@
  * @author tangzehua
  * @sine 2019-08-16 09:30
  */
-import React from 'react';
+import React, {PureComponent} from 'react';
 import {Route} from '@xt-web/react-dom'
 import { hot } from 'react-hot-loader/root';
+import ReactRuler from 'mb-sketch-ruler'
 
-const Home = function (props){
-    return (
-        <div>
-            <button onClick={()=>Route.push("/test")}>调整到123</button>
-        </div>
-    )
-};
+const thick = 16;
+
+ class Home extends PureComponent {
+    state = {
+        scale: 2, //658813476562495, //1,
+        startX: 0,
+        startY: 0,
+        lines: {
+            h: [100, 200],
+            v: [100, 200]
+        }
+    };
+
+    componentDidMount () {
+        // 滚动居中
+        this.$app.scrollLeft = this.$container.getBoundingClientRect().width / 2 - 300 // 300 = #screens.width / 2
+    }
+    componentDidUpdate (prevProps, prevState) {
+        if (this.state.scale !== prevState.scale) {
+            this.handleScroll()
+        }
+    }
+
+    setAppRef = ref => this.$app = ref
+    setContainerRef = ref => this.$container = ref
+
+    handleScroll = () => {
+        const screensRect = document.querySelector('#screens').getBoundingClientRect()
+        const canvasRect = document.querySelector('#canvas').getBoundingClientRect()
+
+        // 标尺开始的刻度
+        const { scale } = this.state
+        const startX = (screensRect.left + thick - canvasRect.left) / scale
+        const startY = (screensRect.top + thick - canvasRect.top) / scale
+        this.setState({ startX, startY })
+    }
+    handleWheel = (e) => {
+        if (e.ctrlKey || e.metaKey) {
+            e.preventDefault()
+            const nextScale = parseFloat(Math.max(0.2, this.state.scale - e.deltaY / 500).toFixed(2))
+            this.setState({ scale: nextScale })
+        }
+    }
+    handleLine = (lines) => {
+        this.setState({ lines })
+    }
+    render () {
+        const { scale, startX, startY, lines } = this.state
+        const { h, v } = lines
+
+        const rectWidth = 160
+        const rectHeight = 200
+
+        const canvasStyle = {
+            width: rectWidth,
+            height: rectHeight,
+            transform: `scale(${scale})`
+        }
+        const shadow = {
+            x: 0,
+            y: 0,
+            width: rectWidth,
+            height: rectHeight
+        }
+
+        return (
+            <div className="wrapper">
+                <div className="scale-value">{`scale: ${scale}`}</div>
+                <ReactRuler
+                    thick={thick}
+                    scale={scale}
+                    width={582}
+                    height={482}
+                    startX={startX}
+                    startY={startY}
+                    shadow={shadow}
+                    horLineArr={h}
+                    verLineArr={v}
+                    handleLine={this.handleLine}
+                    cornerActive={true}
+                    onCornerClick={this.handleCornerClick}
+                />
+                <div ref={this.setAppRef} id="screens" onScroll={this.handleScroll} onWheel={this.handleWheel}>
+                    <div ref={this.setContainerRef} className="screen-container">
+                        <div id="canvas" style={canvasStyle} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default hot(Home);
 
