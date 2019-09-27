bff7e1dba9c888a56acb48fb9a885fd3f9a62c29
tangzehua <stulip@126.com>
2019-09-27 11:06:15 +0800

    form 4

--- modules/fr-design/src/config/Attribute.js
+++ modules/fr-design/src/config/Attribute.js
@@ -4,4 +4,51 @@
  * @sine 2019-09-24 16:17
  */
 
+// 布局方向常量
+export const ArrangeConst = {
+    alignTop: 1,
+    alignRight: 2,
+    alignBottom: 3,
+    alignLeft: 4,
+    evenlyH: 5,
+    evenlyV: 6,
+    alignCenterV: 7,
+    alignCenterH: 8,
+};
 
+// 布局按钮配置
+export const ArrangeConfig = [
+    {
+        type: ArrangeConst.evenlyH,
+        icon: 'design/dist_evenly_h',
+    },
+    {
+        type: ArrangeConst.evenlyV,
+        icon: 'design/dist_evenly_v',
+    },
+    {
+        type: ArrangeConst.alignLeft,
+        icon: 'design/align_left',
+    },
+    {
+        type: ArrangeConst.alignCenterV,
+        icon: 'design/align_center_v',
+    },
+    {
+        type: ArrangeConst.alignRight,
+        icon: 'design/align_right',
+    },
+    {
+        type: ArrangeConst.alignTop,
+        icon: 'design/align_top',
+    },
+    {
+        type: ArrangeConst.alignCenterH,
+        icon: 'design/align_center_h',
+    },
+    {
+        type: ArrangeConst.alignBottom,
+        icon: 'design/align_bottom',
+    },
+
+];
--- modules/fr-design/src/pages/design/view/Exterior.jsx
+++ modules/fr-design/src/pages/design/view/Exterior.jsx
@@ -5,42 +5,53 @@
  */
 
 // @flow
-import * as React from 'react';
-import '../assets/exterior.pcss'
-import {Form} from 'fr-ui'
+import * as React from "react";
+import "../assets/exterior.pcss";
+import { Form } from "fr-ui";
+import { IBotSVG } from "fr-web";
+import { ArrangeConfig } from "../../../config/Attribute";
 
-type Props = {
-
-};
-type State = {
-
-};
+type Props = {};
+type State = {};
 
 const BasisConfig = [
     {
-        title: '高度',
-        form: 'width',
-        type: 'text',
-        value: '测试测试,,,,,测试'
+        title: "高度",
+        form: "width",
+        type: "text",
+        value: "测试测试,,,,,测试"
     },
     {
-        title: '宽度',
-        form: 'height',
-        type: 'text',
-        value: '测试测试,,,,,测试'
+        title: "宽度",
+        form: "height",
+        type: "text",
+        value: "测试测试,,,,,测试"
     }
 ];
 
 export class Exterior extends React.Component<Props, State> {
-    _render() {
+    renderArrange = item => {
         return (
-            <div className={'appearance-panel'}>
-                <Form.View config={BasisConfig}/>
-            </div>
+            <a className={"item"}>
+                <IBotSVG name={item.icon} />
+            </a>
         );
     };
 
-    render(){
+    _render() {
+        return (
+            <>
+                <div className={"arrange"}>{ArrangeConfig.map(this.renderArrange)}</div>
+                <main>
+                    <div className={"appearance-panel"}>
+                        <Form.View config={BasisConfig} />
+                    </div>
+                </main>
+            </>
+        );
+    }
+
+    render() {
         return this._render();
     }
 }
