77afdb36d68ee7c72e1bb72533ae653c61683208
tangzehua <stulip@126.com>
2019-10-11 11:12:02 +0800

    调整component to widget folder

--- modules/fr-design/src/pages/design/view/ViewGroup.jsx
+++ modules/fr-design/src/pages/design/view/ViewGroup.jsx
@@ -6,7 +6,7 @@
 
 // @flow
 import React from 'react';
-import {StatusBar, Header, IPhoneXOperateBar} from "../../../components";
+import {StatusBar, Header, IPhoneXOperateBar} from "../../../widget";
 import {observer} from "mobx-react";
 import {classNames} from 'fr-web'
 import '../../../components/assets'
@@ -21,7 +21,7 @@ type State = {
 @observer
 export class ViewGroup extends React.Component<Props, State> {
 
-    render() {
+    _render() {
         const {main} = this.props.store;
         const {canvasRect, canvasScale} = main.section;
         const {designRect} = main.config;
@@ -31,7 +31,63 @@ export class ViewGroup extends React.Component<Props, State> {
                 <StatusBar width={canvasRect.width} height={designRect.top}/>
                 <Header width={canvasRect.width} height={designRect.nav_height}/>
                 <IPhoneXOperateBar width={canvasRect.width} height={designRect.bottom} designHeight={canvasRect.height}/>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
+                <div>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                    <span>测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试</span>
+                </div>
             </div>
         );
     };
+
+    render () {
+        return this._render();
+    }
 }
---
+++ modules/fr-design/src/widget/base/BaseWidget.jsx
@@ -0,0 +1,5 @@
+/**
+ *
+ * @author tangzehua
+ * @sine 2019-10-11 11:02
+ */
