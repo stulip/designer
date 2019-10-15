ce9c8235ae443dee42249da4ae9ccde89387a471
侯治涛 <houzhitao1010@gmail.com>
2019-10-15 19:26:34 +0800

    偏好设置 最后的功能， 明天来调整

--- modules/fr-design/src/pages/design/store/FooterStore.js
+++ modules/fr-design/src/pages/design/store/FooterStore.js
@@ -4,17 +4,52 @@
  * @author tangzehua
  * @sine 2019-09-05 10:27
  */
-import {observable, action, computed} from 'mobx';
+import {observable, action, computed, toJS} from 'mobx';
 import type {MainStore} from "../../../flow/Main.flow";
 import {BaseStore} from "./BaseStore";
 
+type ThemeCube = {
+    type: string,
+    color: string,
+    class: string
+}
+
+type PreferenceConfig = {
+    theme: string,
+    isScale: boolean,
+    isToolbar: boolean
+}
+
 export class FooterStore extends BaseStore {
+
+    themeColor: ThemeCube[] = [
+        { color: "#C8CDD1", class: "white", type: "theme" },
+        { color: "#c8cdd0", class: "grey", type: "theme" }
+    ];
+
+    @observable
+    _preferenceConfig: PreferenceConfig = {
+        theme: "white",
+        isScale: true,
+        isToolbar: true
+    };
+
     @observable
     showSetting: boolean = false;
 
     @action
     setPreferenceAction = () => {
         this.showSetting = !this.showSetting;
+    };
+
+    @action
+    setPreferenceConfig = (key, value) => {
+        this._preferenceConfig[key] = value;
+    };
+
+    @computed
+    get preferenceConfig(): PreferenceConfig {
+        return toJS(this._preferenceConfig);
     }
 
 }
--- modules/fr-design/src/pages/design/view/Footer.jsx
+++ modules/fr-design/src/pages/design/view/Footer.jsx
@@ -30,16 +30,35 @@ export class Footer extends React.Component<Props, State> {
     }
 
     render() {
+        const { themeColor, preferenceConfig, showSetting, setPreferenceAction, setPreferenceConfig } = this.props.store;
+        console.log(this.props.store, "---sdf");
         return (
             <div className={'ds-footer'}>
                 <span>网页、App设计</span>
-                <span className={"preference-setting"} >
+                <span className={`preference-setting ${showSetting ? "active": ""}`} onClick={setPreferenceAction}>
                     <IBotSVG icon={undefined} name="design/preference" />
                     偏好设置
                 </span>
-                <div className={'setting-plate'}>
-                    设置具体内容，明天来写！！！
-                </div>
+                { showSetting && <div className={'setting-plate'}>
+                        <div className={"plate"}>
+                            <span>主题</span>
+                            <span>{themeColor.map(kk => <b key={kk.class} className={`square ${kk.class} ${kk.class == preferenceConfig[kk.type] ? "is-active": "" }`} onClick={() => setPreferenceConfig(kk.type, kk.class)} />)}</span>
+                        </div>
+                        <div className={"plate"}>
+                            <span>标尺</span>
+                            <label className={`switch ${preferenceConfig.isScale ? "is-checked": ""}`} onClick={() => setPreferenceConfig("isScale", !preferenceConfig.isScale)}>
+                                <strong></strong>
+                            </label>
+                        </div>
+
+                        <span style={{ marginTop: "10px"}}>工具栏文字</span>
+                        <div className={"plate"} style={{ marginTop: "-5px"}}>
+                            <span className={"sub-title"}>显示工具栏工具标签文字</span>
+                            <label className={`switch ${preferenceConfig.isToolbar ? "is-checked": ""}`} onClick={() => setPreferenceConfig("isToolbar", !preferenceConfig.isToolbar)}>
+                                <strong></strong>
+                            </label>
+                        </div>
+                    </div> }
             </div>
         );
     };
